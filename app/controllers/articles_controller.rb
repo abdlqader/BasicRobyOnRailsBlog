d08617911f24e229ae3af628c4cf0e0bea24fef1
abdulkader <abdulkader_hassan74@hotmail.com>
2022-07-08 14:57:25 +0200

    concern implemented on model level , DRYing the status of both models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,23 +2,28 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
-    end 
+    end
   end
+
   def edit
     @article = Article.find(params[:id])
   end
+
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -27,13 +32,16 @@ class ArticlesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to root_path, status: :see_other
   end
+
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end 
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
 end
